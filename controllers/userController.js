b7d26fc6bc3e3ee00200675512f4977a4fe76d4d
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-11-15 15:32:01 -0500

    created show route and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,19 @@
 const User = require('../models/User')
 
 const userController = {
-    Index: (req, res) => {
+    index: (req, res) => {
         User.find({})
             .then((users) => {
                 res.send(users)
             })
+    },
+    show: (req, res) => {
+        User.findById(req.params.userId)
+            .then((user) => {
+                res.send(user)
+            })
     }
+
 }
 
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.get('/api/users', userController.index)
+// router.post('/api/users', userController.create)
+router.get('/api/users/:userId', userController.show)
+
+module.exports = router
