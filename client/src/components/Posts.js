c201bcda81d7baa060380214585f04932cd11079
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-11-19 09:33:29 -0500

    updated idea controller route

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import styled from 'styled-components'
+import Post from './Post'
 
 class Posts extends Component {
+    state = {
+
+    }
+
+    componentWillMount() {
+        
+    }
+
     render() {
         return (
             <div>
-                posts
+                
             </div>
         );
     }
--- controllers/postController.js
+++ controllers/postController.js
@@ -6,6 +6,7 @@ const postController = {
         let userId = req.params.userId
         User.findById(userId).populate('posts')
             .then((user) => {
+                user.posts = user.posts.reverse
                 res.send(user.posts)
             })
     },
