5d3883a9a1aa978d165e3400eee6f339d70184b6
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-11-20 23:42:58 -0500

    imported api

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import styled from 'styled-components'
+// import Image from 'react-image-resizer'
+
+const ImgStyles = styled.div`
+    
+`
 
 class UserProfile extends Component {
+    state = {
+        users: [],
+        memes: []
+    }
+
+    getAllMemes = () => {
+        axios.get('https://api.imgflip.com/get_memes').then(res => {
+            console.log(res)
+            console.log(res.data.data.memes)
+            this.setState(
+                {
+                    memes: res.data.data.memes
+                }
+            )
+        })
+    }
+
+
+    // getAllMemes = () => {
+    //     axios.get('https://api.imgflip.com/get_memes')
+    //     .then(res => res.data.data.memes.map(meme => (
+    //         {
+    //             name: `${meme.name}`
+    //         }
+    //     )))
+    //     .then(newData => console.log(newData))
+    // }
+
+    componentDidMount() {
+        this.getAllMemes()
+    }
     render() {
         return (
             <div>
-                
+                {this.state.memes.map(meme => (
+                    <div key={meme.id}>
+                        {console.log('meme')}
+                        {/* <Image>
+                            <img src={meme.url} 
+                            alt={meme.name}
+                            height={50}
+                            width={50} />
+                        </Image> */}
+                        <img src={meme.url}
+                        alt={meme.name}
+                        />
+                        
+                    </div>
+                ))}
             </div>
         );
     }
